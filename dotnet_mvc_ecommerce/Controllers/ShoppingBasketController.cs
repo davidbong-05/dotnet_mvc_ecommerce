3e760a46125dbac2d7eb93035c80df771eb17883
davidbong-05 <davidbong05@gmail.com>
2023-06-09 14:36:37 +0800

    build: combined shopping basket and my basket

--- dotnet_mvc_ecommerce/Controllers/ShoppingBasketController.cs
+++ dotnet_mvc_ecommerce/Controllers/ShoppingBasketController.cs
@@ -7,16 +7,22 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using dotnet_mvc_ecommerce.Data;
 using dotnet_mvc_ecommerce.Models;
+using Microsoft.AspNetCore.Identity;
+using dotnet_mvc_ecommerce.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
 
 namespace dotnet_mvc_ecommerce.Controllers
 {
+    [Authorize]
     public class ShoppingBasketController : Controller
     {
         private readonly dotnet_mvc_ecommerceContext _context;
+        private readonly UserManager<User> _userManager;
 
-        public ShoppingBasketController(dotnet_mvc_ecommerceContext context)
+        public ShoppingBasketController(dotnet_mvc_ecommerceContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: ShoppingBasket
@@ -166,6 +172,65 @@ namespace dotnet_mvc_ecommerce.Controllers
         private bool ShoppingBasketExists(int id)
         {
           return (_context.ShoppingBasket?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        [HttpPost, ActionName("remove")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> remove(int id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var product = await _context.Product.FindAsync(id);
+            var shoppingBasket = await _context.ShoppingBasket.FirstOrDefaultAsync(m => m.UserId == user.Id);
+            var quantity = 1;
+            if (product != null)
+            {
+                var shoppingBasketProduct = await _context.ShoppingBasket_Product.FirstOrDefaultAsync(p =>
+                    p.ShoppingBasketId == shoppingBasket.Id && p.ProductId == product.Id);
+
+                if (shoppingBasketProduct.Quantity >= 0)
+                {
+                    shoppingBasketProduct.Quantity -= quantity;
+                }
+
+                if (shoppingBasketProduct.Quantity == 0)
+                {
+                    _context.ShoppingBasket_Product.Remove(shoppingBasketProduct);
+                }
+
+                await _context.SaveChangesAsync();
+                TempData["success"] = "Product added to basket";
+            }
+
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost, ActionName("add")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> add(int id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var product = await _context.Product.FindAsync(id);
+            var shoppingBasket = await _context.ShoppingBasket.FirstOrDefaultAsync(m => m.UserId == user.Id);
+            var quantity = 1;
+
+            if (product != null)
+            {
+                var shoppingBasketProduct = await _context.ShoppingBasket_Product.FirstOrDefaultAsync(p =>
+                    p.ShoppingBasketId == shoppingBasket.Id && p.ProductId == product.Id);
+
+                if (shoppingBasketProduct.Quantity >= 1)
+                {
+                    shoppingBasketProduct.Quantity += quantity;
+                }
+
+                await _context.SaveChangesAsync();
+                TempData["success"] = "Product added to basket";
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
